1a33a9c709cf3bae1221cacaae5c0de11e1b9827
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-06-18 00:28:41 +0700

    feat: menambahkan list command dari kode yang sebelumnya

--- src/handler/message.ts
+++ src/handler/message.ts
@@ -15,6 +15,50 @@ export const messageHandler =
       .toLowerCase();
 
     switch (command) {
+      case "c":
+      case "cg":
+      case "create":
+      case "creategame":
+        break;
+      case "s":
+      case "sg":
+      case "start":
+      case "startgame":
+        break;
+      case "j":
+      case "jg":
+      case "join":
+      case "joingame":
+        break;
+      case "i":
+      case "ig":
+      case "info":
+      case "infogame":
+        break;
+      case "eg":
+      case "end":
+      case "endgame":
+        break;
+
+      case "l":
+      case "lg":
+      case "quit":
+      case "leave":
+      case "leavegame":
+        break;
+      case "p":
+      case "play":
+        break;
+      case "say":
+        break;
+      case "cards":
+        break;
+      case "d":
+      case "pickup":
+      case "newcard":
+      case "draw":
+        break;
+
       default: {
         await sock.sendMessage(WebMessage.key.remoteJid!, {
           text:
