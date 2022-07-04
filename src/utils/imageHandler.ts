e0e59a680484daf072f300b181719175ea6214f6
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-07-04 21:58:31 +0700

    feat: menambahkan fitur caching gambar

--- src/utils/imageHandler.ts
+++ src/utils/imageHandler.ts
@@ -1,9 +1,11 @@
 import fs from "fs";
 import path from "path";
 import sharp from "sharp";
+import NodeCache from "node-cache";
 
 import type { allCard } from "../config/cards";
 
+const imgCache = new NodeCache({ stdTTL: 60 * 60 * 24 });
 const cardsDir = path.join(path.resolve(), "assets/images/cards");
 
 const cardBuilder = (width: number, height: number) =>
@@ -20,7 +22,10 @@ export const getCardImage = (card: allCard) =>
   fs.readFileSync(path.join(cardsDir, `${card}.png`));
 
 export const createCardsImageFront = async (cards: allCard[]) => {
-  return await cardBuilder(cards.length * 95, 137)
+  const cacheKeyName = `front-${cards.join("-")}`;
+  if (imgCache.has(cacheKeyName)) return imgCache.get(cacheKeyName) as Buffer;
+
+  const img = await cardBuilder(cards.length * 95, 137)
     .composite(
       cards.map((card, idx) => ({
         input: path.join(cardsDir, `${card}.png`),
@@ -30,10 +35,16 @@ export const createCardsImageFront = async (cards: allCard[]) => {
     )
     .toFormat("png")
     .toBuffer();
+
+  imgCache.set(cacheKeyName, img, 60 * 20);
+  return img;
 };
 
 export const createCardsImageBack = async (cardsLength: number) => {
-  return await cardBuilder(cardsLength * 95 - 10, 135)
+  const cacheKeyName = `back${cardsLength}`;
+  if (imgCache.has(cacheKeyName)) return imgCache.get(cacheKeyName) as Buffer;
+
+  const img = await cardBuilder(cardsLength * 95 - 10, 135)
     .composite(
       Array.from(new Array(cardsLength)).map((_, idx) => ({
         input: path.join(cardsDir, "back.png"),
@@ -43,6 +54,9 @@ export const createCardsImageBack = async (cardsLength: number) => {
     )
     .toFormat("png")
     .toBuffer();
+
+  imgCache.set(cacheKeyName, img);
+  return img;
 };
 
 export const createAllCardImage = async (
